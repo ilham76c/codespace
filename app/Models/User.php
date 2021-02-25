1b33c76da1349a820a2d16e30c1426271bd40a39
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-02-26 00:00:18 +0700

    membuat model dan migration table spaces dan table space_photos

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function spaces()
+    {
+        return $this->hasMany(Space::class, 'user_id', 'id');
+    }
 }
