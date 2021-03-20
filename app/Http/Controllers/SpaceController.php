363244c5a9294a7668afe3f7ca083e844c4acdb8
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-03-21 00:32:42 +0700

    membuat fungsi untuk upload dan delete file photo

--- app/Http/Controllers/SpaceController.php
+++ app/Http/Controllers/SpaceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Space;
+use Illuminate\Support\Facades\Storage;
 
 class SpaceController extends Controller
 {
@@ -45,10 +46,24 @@ class SpaceController extends Controller
             'address' => 'required|min:5',
             'description' => 'required|min:10',
             'latitude' => 'required',
-            'longitude' => 'required'
+            'longitude' => 'required',
+            'photo' => 'required',
+            'photo.*' => 'mimes:jpg,png'
         ]);
 
-        $request->user()->spaces()->create($request->all());
+        $space = $request->user()->spaces()->create($request->except('photo'));
+
+        $spacePhotos = [];
+
+        foreach($request->file('photo') as $file) {
+            $path = Storage::disk('public')->putFile('space', $file);
+            $spacePhotos[] = [
+                'space_id' => $space->id,
+                'path' => $path
+            ];
+        }
+
+        $space->photos()->insert($spacePhotos);
 
         return redirect()->route('space.index')->with('status', 'Space created!');
     }
@@ -117,6 +132,10 @@ class SpaceController extends Controller
         $space = Space::findOrFail($id);
         if ($space->user_id != request()->user()->id) {
             return redirect()->back();
+        }
+
+        foreach($space->photos as $photo) {
+            Storage::disk('public')->delete($photo->path);
         }
 
         $space->delete();
