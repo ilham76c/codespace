4203eb1a19b0f2c16aaf3384c4a33154cbf5a204
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-03-21 10:42:35 +0700

    menampilkan photo di codespace

--- resources/views/pages/space/show.blade.php
+++ resources/views/pages/space/show.blade.php
@@ -7,13 +7,41 @@
         <div class="col-md-8">
             <div class="card mb-3">
                 <div class="card-header">Space: {{ $space->title }}</div>
-
                 <div class="card-body">
-                  <div id="mapContainer" style="height: 500px">
-
-                  </div>
+                  <div id="mapContainer" style="height: 500px"></div>
                 </div>                
             </div>
+
+            <div class="card">
+              <div class="card-body">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    @foreach ($space->photos as $key => $photo)
+                      <li data-target="#carouselExampleIndicators" data-slide-to="{{ $key }}" class="{{ $key == 0 ? 'active' : '' }}"></li>
+                    @endforeach
+                  </ol>
+                  <div class="carousel-inner">
+                    @foreach ($space->photos as $key => $photo)
+                      <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
+                        <img src="{{ asset('storage/' . $photo->path) }}" class="d-block w-100" alt="gambar">
+                      </div>
+                    @endforeach
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+
             <div class="card mb-3">
               <div class="card-body">
                 <h3>{{ $space->title }}</h3>
