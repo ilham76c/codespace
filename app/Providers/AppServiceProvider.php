085034cb01329f56854da6cb26f94965bc7df778
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-03-23 20:16:54 +0700

    mengganti route() dengan secure_url() pada action form login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // $this->app->bind('path.public', function() {
+        //     return realpath(base_path().'../public');
+        // });
     }
 
     /**
@@ -27,5 +29,8 @@ class AppServiceProvider extends ServiceProvider
         if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
             URL::forceScheme('https');
         }
+        // if ($this->app->environment('production')) {
+        //     \URL::forceScheme('https');
+        // }
     }
 }
